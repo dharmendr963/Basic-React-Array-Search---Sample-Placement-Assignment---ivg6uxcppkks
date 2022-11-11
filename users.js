02d98306570240e0a02c04ec9d06070aab00c764
dharmendr963 <114353361+dharmendr963@users.noreply.github.com>
2022-11-11 06:08:41 -0800

    users.js
    code

---
+++ users.js
@@ -0,0 +1,15 @@
+@@ -13,4 +13,13 @@ router.get('/',  (req, res) =>{
+}) 
+
+
+module.exports = router;
+module.exports = router;
+
+
+
+// userid
+// name
+// username
+// email
+// mobile
+// password
